7eefbe7d7df81e48798bfa07f041982aa298658f
Tanay Bhardwaj <43832878+Tanay0510@users.noreply.github.com>
2021-06-21 10:35:48 -0400

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -45,12 +45,12 @@ def clean_data(df):
 	categories.columns = category_colnames
 
 	for column in categories:
-    	# set each value to be the last character of the string
-    	
-		categories[column] = categories[column].str[-1]
+    # set each value to be the last character of the string
+    		categories[column] = categories[column].apply(lambda x: int(x.split('-')[1]))
     
-    	# convert column from string to numeric
-		categories[column] = categories[column].astype(int)
+    # convert column from string to numeric
+    		categories[column] = categories[column].astype(int)
+
     	
     # drop the original categories column from `df`
 	
